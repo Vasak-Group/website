aeb0ba94f82f1ea14a71d4abef4e01a1655d1978
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-02-24 12:28:50 -0300

    Basic Example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './sections/home/home.component';
+import { NavbarComponent } from './partial/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: NavbarComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
