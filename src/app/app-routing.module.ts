f6d88e5b0a5a03832eebf2d933cc686485cd2764
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-09-16 21:34:15 -0300

    Add Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './sections/home/home.component';
 import { PrivacidadComponent } from './sections/privacidad/privacidad.component';
+import { ProductsComponent } from './sections/products/products.component';
 
 const routes: Routes = [
   { path: 'privacidad', component: PrivacidadComponent },
+  { path: 'productos', component: ProductsComponent },
   { path: '', component: HomeComponent }
 ];
 
