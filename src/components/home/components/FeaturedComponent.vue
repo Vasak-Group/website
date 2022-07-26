74fbb645931a859beb0bee973aeb26536f5a075b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-07-26 11:06:01 -0300

    Fix featureds

---
+++ src/components/home/components/FeaturedComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="icon-box text-center" data-aos="fade-up" :data-aos-delay="delay">
+        <div class="icon"><i :class="icon"></i></div>
+        <h4 class="title"><a href="">{{ title }}</a></h4>
+        <p class="description">{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        delay: {
+            type: Number,
+            default: 100
+        }
+    }
+}
+</script>
+
+<style>
+</style>
