9853b51a8e1884fa410c8a65569deaaa93a83f05
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-07-26 15:02:03 -0300

    Update Contact

--- src/components/home/ContactHome.vue
+++ src/components/home/ContactHome.vue
@@ -3,34 +3,33 @@
         <div class="container" data-aos="fade-up">
 
             <div class="section-title">
-                <h2>Contact</h2>
-                <h3><span>Contact Us</span></h3>
-                <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque
-                    vitae autem.</p>
+                <h2>Contacto</h2>
+                <h3><span>Comunicate</span></h3>
+                <p>Comunicate con nosotros. Comenza hoy a potenciar tus ideas.</p>
             </div>
 
             <div class="row" data-aos="fade-up" data-aos-delay="100">
                 <div class="col-lg-6">
                     <div class="info-box mb-4">
-                        <i class="bx bx-map"></i>
-                        <h3>Our Address</h3>
-                        <p>A108 Adam Street, New York, NY 535022</p>
+                        <i class="fa fa-map"></i>
+                        <h3>Encuentranos</h3>
+                        <p>Argentina, Buenos aires, GBA Sur (Quilmes - Berazategui)</p>
                     </div>
                 </div>
 
                 <div class="col-lg-3 col-md-6">
                     <div class="info-box  mb-4">
-                        <i class="bx bx-envelope"></i>
-                        <h3>Email Us</h3>
-                        <p>contact@example.com</p>
+                        <i class="fa fa-envelope"></i>
+                        <h3>Email</h3>
+                        <p>indo@lynx.net.ar</p>
                     </div>
                 </div>
 
                 <div class="col-lg-3 col-md-6">
                     <div class="info-box  mb-4">
-                        <i class="bx bx-phone-call"></i>
-                        <h3>Call Us</h3>
-                        <p>+1 5589 55488 55</p>
+                        <i class="fa fa-whatsapp"></i>
+                        <h3>Contactanos</h3>
+                        <p>+54 9 11 2388-2753</p>
                     </div>
                 </div>
 
@@ -38,39 +37,10 @@
 
             <div class="row" data-aos="fade-up" data-aos-delay="100">
 
-                <div class="col-lg-6 ">
+                <div class="col-12 ">
                     <iframe class="mb-4 mb-lg-0"
-                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13649.517448918608!2d-58.263308295685945!3d-34.76757433099286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1658857845590!5m2!1ses-419!2sar"
                         frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
-                </div>
-
-                <div class="col-lg-6">
-                    <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                        <div class="row">
-                            <div class="col form-group">
-                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name"
-                                    required>
-                            </div>
-                            <div class="col form-group">
-                                <input type="email" class="form-control" name="email" id="email"
-                                    placeholder="Your Email" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"
-                                required>
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control" name="message" rows="5" placeholder="Message"
-                                required></textarea>
-                        </div>
-                        <div class="my-3">
-                            <div class="loading">Loading</div>
-                            <div class="error-message"></div>
-                            <div class="sent-message">Your message has been sent. Thank you!</div>
-                        </div>
-                        <div class="text-center"><button type="submit">Send Message</button></div>
-                    </form>
                 </div>
 
             </div>
@@ -85,5 +55,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+iframe, .info-box {
+    border-radius: var(--border-radius);
+}
 </style>
