7ac8355ab8e4327fd44d4eadd3012abaa606bfd3
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-07-27 13:53:38 -0300

    Add Members

--- src/components/home/TeamHome.vue
+++ src/components/home/TeamHome.vue
@@ -3,84 +3,16 @@
         <div class="container" data-aos="fade-up">
 
             <div class="section-title">
-                <h2>Team</h2>
-                <h3>Our Hardworking <span>Team</span></h3>
-                <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque
-                    vitae autem.</p>
+                <h2>Equipo</h2>
+                <h3>Conocé a nuestro <span>equipo</span></h3>
+                <p>Conocé a las personas que hacen posible todo esto. Y se preocupan por hacer crecer tus proyectos.</p>
             </div>
 
             <div class="row">
 
-                <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member">
-                        <div class="member-img">
-                            <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div class="member-info">
-                            <h4>Walter White</h4>
-                            <span>Chief Executive Officer</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
-                    <div class="member">
-                        <div class="member-img">
-                            <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div class="member-info">
-                            <h4>Sarah Jhonson</h4>
-                            <span>Product Manager</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
-                    <div class="member">
-                        <div class="member-img">
-                            <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div class="member-info">
-                            <h4>William Anderson</h4>
-                            <span>CTO</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
-                    <div class="member">
-                        <div class="member-img">
-                            <img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div class="member-info">
-                            <h4>Amanda Jepson</h4>
-                            <span>Accountant</span>
-                        </div>
-                    </div>
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch" v-for="member in team" v-bind:key="member.id"
+                    data-aos="fade-up" :data-aos-delay="member.delay">
+                    <MemberComponent v-bind="member" />
                 </div>
 
             </div>
@@ -90,8 +22,15 @@
 </template>
 
 <script>
+import team from '../../data/team.json'
+import MemberComponent from './components/MemberComponent.vue'
 export default {
-
+    data() {
+        return {
+            team
+        };
+    },
+    components: { MemberComponent }
 }
 </script>
 
---
+++ src/components/home/components/MemberComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="member">
+        <div class="member-img">
+            <img :src="image" class="img-fluid" :alt="name" :title="name" />
+            <div class="social">
+                <a v-for="red in social" :key="red.id" :href="red.url"><i :class="red.icon"></i></a>
+            </div>
+        </div>
+        <div class="member-info">
+            <h4>{{ name }}</h4>
+            <span>{{ position }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        image: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        position: {
+            type: String,
+            required: true
+        },
+        social: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+</style>
