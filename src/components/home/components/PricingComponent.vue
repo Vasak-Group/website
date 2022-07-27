2106e535bdf6e6d29e4223347f8aa7a5ce4be451
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-07-27 14:54:56 -0300

    Add Pricing

---
+++ src/components/home/components/PricingComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="box featured">
+        <h3>{{ nombre }}</h3>
+        <h4><i :class="icon"></i></h4>
+        <ul>
+            <li v-for="caracteristica in caracteristicas" :key="caracteristica.id" :class="caracteristica.class">
+                {{ caracteristica.desc }}
+            </li>
+        </ul>
+        <div class="btn-wrap">
+            <a :href="url" class="btn-buy">Consultanos</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nombre: {
+            type: String,
+            required: true
+        },
+        caracteristicas: {
+            type: Array,
+            required: true
+        },
+        icon: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+</style>
