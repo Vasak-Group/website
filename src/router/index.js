9cc6f9081b82b656a9c3133c1f2d62714036ea50
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-05 10:39:18 -0300

    fix code style

--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,19 @@ import HomeView from '../views/HomeView.vue';
 import ProjectView from '../views/ProjectView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/project/:id',
-      name: 'project',
-      component: ProjectView
-    }
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: HomeView
+		},
+		{
+			path: '/project/:id',
+			name: 'project',
+			component: ProjectView
+		}
+	]
 });
 
-export default router
+export default router;
