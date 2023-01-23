733045aa0a3ab197e604568acb289439bfbeb736
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-01-23 11:09:26 -0300

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -15,5 +15,5 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Vasak-Group/website.git master:gh-pages
+git push -f git@github.com:Vasak-Group/website.git main:gh-pages
 cd -
