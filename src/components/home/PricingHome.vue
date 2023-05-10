842c0a07d2ed71d70809e9132bf50aace93aed91
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-05-10 18:02:14 -0300

    Add new Precing Section

--- src/components/home/PricingHome.vue
+++ src/components/home/PricingHome.vue
@@ -1,21 +1,34 @@
 <template>
-    <section id="pricing" class="pricing bg-body">
-        <div class="container" data-aos="fade-up">
-
-            <div class="section-title">
-                <h2>Planes</h2>
-                <h3>Compare nuestros <span>Planes</span></h3>
-                <p class="text-body">Cada uno de nuestros planes se puede modificar todo lo necesario para sastifacer sus necesidades </p>
-            </div>
-
+    <section id="pricing" class="pricing-section pt-140 pb-100">
+        <div class="container">
             <div class="row">
-
-                <div v-for="price in prices" :key="price.id" class="col-lg-4 col-md-12 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
-                    <PricingComponent v-bind="price" />
+                <div class="col-xl-6 col-lg-7 col-md-10 mx-auto">
+                    <div class="section-title text-center mb-60">
+                        <span class="wow fadeInDown" data-wow-delay=".2s"
+                            style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInDown;">
+                            Planes
+                        </span>
+                        <h2 class="wow fadeInUp" data-wow-delay=".4s"
+                            style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                            Compare nuestros Planes
+                        </h2>
+                        <p class="wow fadeInUp" data-wow-delay=".6s"
+                            style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInUp;">
+                            Cada uno de nuestros planes se puede modificar todo lo necesario para sastifacer sus
+                            necesidades
+                        </p>
+                    </div>
                 </div>
-
             </div>
+            <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-1" role="tabpanel" aria-labelledby="pills-home-tab">
+                    <div class="row">
 
+                        <PricingComponent v-for="price in prices" :key="price.id" v-bind="price" />
+
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -34,5 +47,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/home/components/PricingComponent.vue
+++ src/components/home/components/PricingComponent.vue
@@ -1,16 +1,14 @@
 <template>
-    <div class="box featured bg-body-tertiary">
-        <h3>{{ nombre }}</h3>
-        <h4>
-            <em :class="icon"></em>
-        </h4>
-        <ul>
-            <li v-for="caracteristica in caracteristicas" :key="caracteristica.id" :class="caracteristica.class">
-                {{ caracteristica.desc }}
-            </li>
-        </ul>
-        <div class="btn-wrap bg-body-tertiary">
-            <a :href="url" class="btn-buy">Consultanos</a>
+    <div class="col-xl-4 col-lg-4 col-md-6">
+        <div class="single-pricing mb-50" :class="class">
+            <h4>{{ nombre }}</h4>
+            <h3><em :class="icon"></em></h3>
+            <ul>
+                <li v-for="caracteristica in caracteristicas" :key="caracteristica.id" :class="caracteristica.class">
+                    {{ caracteristica.desc }}
+                </li>
+            </ul>
+            <a :href="url" class="theme-btn border-btn">Consultanos</a>
         </div>
     </div>
 </template>
@@ -33,10 +31,13 @@ export default {
         url: {
             type: String,
             required: true
+        },
+        class: {
+            type: String,
+            required: true
         }
     }
 }
 </script>
 
-<style>
-</style>
+<style></style>
