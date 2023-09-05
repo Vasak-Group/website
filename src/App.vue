28eedface75ad7fa800bc4718c2bbf011b063d2d
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-09-05 11:11:07 -0300

    remove deleted componet

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
 import { RouterView } from 'vue-router';
 import Footer from './components/Footer.vue';
 import Header from './components/Header.vue';
-import TopBar from './components/TopBar.vue';
 </script>
 
 <template>
-  <TopBar />
   <Header />
 
   <RouterView />
