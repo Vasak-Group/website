c0fd151cd3059871660db5bb72bcc5def73f8f00
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-09-10 09:54:37 -0300

    Add about route

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import AboutView from '../views/AboutView.vue';
 import ContactView from '../views/ContactView.vue';
 import HomeView from '../views/HomeView.vue';
 import ProjectView from '../views/ProjectView.vue';
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	mode: 'history',
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: HomeView,
-		},
-		{
-			path: '/project/:id',
-			name: 'project',
-			component: ProjectView,
-		},
-		{
-			path: '/contact',
-			name: 'contact',
-			component: ContactView,
-		},
-	],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutView,
+    },
+    {
+      path: "/project/:id",
+      name: "project",
+      component: ProjectView,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: ContactView,
+    },
+  ],
 });
 
 export default router;
