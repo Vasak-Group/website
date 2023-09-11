25e718c3563dc98ea24d54bbe74427c957cd4175
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-09-11 16:19:01 -0300

    clear home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,15 @@
-<script setup>
-import ClientsHome from '../components/home/ClientsHome.vue';
-import ContactHome from '../components/home/ContactHome.vue';
-import CountsHome from '../components/home/CountsHome.vue';
-import FAQHome from '../components/home/FAQHome.vue';
+<script setup lang="ts">
 import FeatureHome from '../components/home/FeatureHome.vue';
 import HeroHome from '../components/home/HeroHome.vue';
-import PortfolioHome from '../components/home/PortfolioHome.vue';
-import PricingHome from '../components/home/PricingHome.vue';
-import ServicesHome from '../components/home/ServicesHome.vue';
-import TechnologiesHome from '../components/home/TechnologiesHome.vue';
+import PricingComponent from '../components/home/PricingComponent.vue';
+import TestimonialsComponent from '../components/global/TestimonialsComponent.vue';
 </script>
 
 <template>
   <HeroHome />
   <main id="main">
     <FeatureHome />
-    <!--<CountsHome />-->
-    <!--<ClientsHome />-->
-    <ServicesHome />
-    <!--<TechnologiesHome />-->
-    <PortfolioHome />
-    <PricingHome />
-    <!--<FAQHome />-->
-    <!--<ContactHome />-->
+    <PricingComponent />
+    <TestimonialsComponent />
   </main>
 </template>
