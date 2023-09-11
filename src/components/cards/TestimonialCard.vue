20af7da859065ed40e6defbce65b129b33c4f1e2
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-09-11 12:39:22 -0300

    add testimonials

---
+++ src/components/cards/TestimonialCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="item tns-item" aria-hidden="true">
+      <div class="single-testimonial">
+          <div class="testimonial-img">
+              <img :src="image" :alt="name" loading="lazy"/>
+          </div>
+          <div class="testimonial-content mb-35">
+              <p class="pb-30">{{ comment }}</p>
+              <div class="testimonial-info">
+                  <div class="testimonial-meta">
+                      <h5>{{ name }}</h5>
+                      <span>{{ position }}</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'TestimonialCard',
+    props: {
+        name: String,
+        position: String,
+        image: String,
+        comment: String,
+    },
+});
+</script>
--- src/components/global/TestimonialsComponent.vue
+++ src/components/global/TestimonialsComponent.vue
@@ -1,40 +1,80 @@
 <template>
-    <section id="testimonials" class="testimonials">
-        <div class="container" data-aos="zoom-in">
-            TEstimonios
+    <section id="testimonials" class="testimonial-section pt-150 pb-150 img-bg">
+        <div class="container">
+            <div class="row align-items-center">
 
-            <div class="testimonials-slider swiper">
-                <div class="swiper-wrapper">
-
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <img src="https://asd.asd/assets/img/testimonials/testimonials-1.jpg"
-                                class="testimonial-img" alt="">
-                            <h3>Saul Goodman</h3>
-                            <h4>Ceo &amp; Founder</h4>
-                            <p>
-                                <em class="bx bxs-quote-alt-left quote-icon-left"></em>
-                                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit
-                                rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam,
-                                risus at semper.
-                                <em class="bx bxs-quote-alt-right quote-icon-right"></em>
-                            </p>
-                        </div>
-                    </div><!-- End testimonial item -->
-
-
+                <div class="col-xl-6 col-lg-10 mx-auto">
+                    <div class="section-title mb-60">
+                        <span v-scroll-reveal.reset>Testimonios</span>
+                        <h2 v-scroll-reveal.reset>Que es lo que nuestros <br> clientes comentan</h2>
+                    </div>
                 </div>
-                <div class="swiper-pagination"></div>
+                
+                <div class="col-xl-6 col-lg-10 mx-auto">
+                        <div class="testimonial-wrapper">
+                            <div class="tns-outer" id="tns2-ow">
+                                <div class="tns-controls" aria-label="Carousel Navigation" tabindex="0">
+                                    <button type="button" data-controls="prev" tabindex="-1" aria-controls="tns2">
+                                        <i class="fa fa-arrow-left"></i>
+                                    </button>
+                                    <button type="button" data-controls="next" tabindex="-1" aria-controls="tns2">
+                                        <i class="fa fa-arrow-right"></i>
+                                    </button>
+                                </div>
+                                <div class="tns-liveregion tns-visually-hidden" aria-live="polite" aria-atomic="true">
+                                    slide <span class="current">3</span>  of 2
+                                </div>
+                                <div id="tns2-mw" class="tns-ovh">
+                                    <div class="tns-inner" id="tns2-iw">
+                                        <div class="testimonial-active tns-slider tns-carousel tns-subpixel tns-calc tns-horizontal" id="tns2" style="transform: translate3d(-33.3333%, 0px, 0px);">
+                                            
+                                            <TestimonialCard 
+                                                v-for="testimonial in testimonials" 
+                                                :name="testimonial.name"
+                                                :position="testimonial.position"
+                                                :image="testimonial.image"
+                                                :comment="testimonial.comment"  />
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
             </div>
 
         </div>
     </section>
 </template>
 
-<script>
-export default {
+<script setup lang="ts">
+import testimonials from '../../data/testimonials.json';
 
-}
+import { tns } from 'tiny-slider';
+import { createScrollRevealDirective } from 'vue-scroll-reveal'; 
+
+import TestimonialCard from '../cards/TestimonialCard.vue';
+
+const vScrollReveal = createScrollRevealDirective({
+    delay: 100,
+    duration: 1500,
+});
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    tns({
+        container: '.testimonial-active',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        mouseDrag: true,
+        gutter: 0,
+        nav: false,
+        controlsText: ['<i class="fa fa-arrow-left"></i>', '<i class="fa fa-arrow-right"></i>'],
+    });
+});
+
 </script>
 
 <style>
