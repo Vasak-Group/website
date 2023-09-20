a2be078d386e498e6ddb07aeaecfccdc2687ccc3
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-09-20 17:14:09 -0300

    se extrae area de contacto

---
+++ src/components/contact/ContactForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form action="https://api.web3forms.com/submit" method="POST" class="contact-form">
+      <div class="row">
+          <div class="col-md-6">
+              <input type="text" name="name" id="name" placeholder="Nombre" disabled required>
+          </div>
+          <div class="col-md-6">
+              <input type="email" name="email" id="email" placeholder="Email" disabled required>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-6">
+              <input type="text" name="phone" id="phone" placeholder="Telefono" disabled required>
+          </div>
+          <div class="col-md-6">
+              <input type="text" name="subject" id="email" placeholder="Motivo" disabled required>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-12">
+              <textarea name="message" id="message" placeholder="Escribe tu Mensaje..." rows="5"
+                  disabled></textarea>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-12">
+              <div class="button text-center">
+                  <button type="submit" class="theme-btn disabled" disabled>Send Message</button>
+              </div>
+          </div>
+      </div>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'ContactForm',
+});
+</script>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -50,68 +50,31 @@
                         <div class="row">
                             <div class="col-xl-10 col-lg-8 mx-auto">
                                 <div class="section-title text-center mb-50">
-                                    <span class="wow fadeInDown" data-wow-delay=".2s"
-                                        style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInDown;">
+                                    <span>
                                         Contacto
                                     </span>
-                                    <h2 class="wow fadeInUp" data-wow-delay=".4s"
-                                        style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                                    <h2>
                                         Comunicate
                                     </h2>
-                                    <p class="wow fadeInUp" data-wow-delay=".6s"
-                                        style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInUp;">
+                                    <p>
                                         Comunicate con nosotros. Comenza hoy a potenciar tus ideas.
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        <form action="assets/php/mail.php" class="contact-form">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <input type="text" name="name" id="name" placeholder="Nombre" disabled required>
-                                </div>
-                                <div class="col-md-6">
-                                    <input type="email" name="email" id="email" placeholder="Email" disabled required>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <input type="text" name="phone" id="phone" placeholder="Telefono" disabled required>
-                                </div>
-                                <div class="col-md-6">
-                                    <input type="text" name="subject" id="email" placeholder="Motivo" disabled required>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12">
-                                    <textarea name="message" id="message" placeholder="Escribe tu Mensaje..." rows="5"
-                                        disabled></textarea>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="button text-center">
-                                        <button type="submit" class="theme-btn disabled" disabled>Send Message</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                        <ContactForm />
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <section class="map-section">
-        <div class="map-container">
-            <object style="border:0; height: 690px; width: 100%;"
-                data="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16835.08026906632!2d-8.635669250422245!3d41.16186463554482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2465abc4e153c1%3A0xa648d95640b114bc!2sOporto%2C%20Portugal!5e0!3m2!1ses-419!2sar!4v1683810190448!5m2!1ses-419!2sar">
-            </object>
-        </div>
-    </section>
+    <MapComponent />
 </template>
 
 <script setup lang="ts">
-import BreadcrumbsComponent from '../components/global/BreadcrumbsComponent.vue';
+import ContactForm from '@/components/contact/ContactForm.vue';
+import MapComponent from '@/components/contact/MapComponent.vue';
+import BreadcrumbsComponent from '@/components/global/BreadcrumbsComponent.vue';
 import { defineComponent, onMounted } from 'vue';
 
 onMounted(() => {
@@ -123,6 +86,8 @@ defineComponent({
   name: 'ContactView',
   components: {
     BreadcrumbsComponent,
+    MapComponent,
+    ContactForm
   },
 });
 </script>
