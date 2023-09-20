8452396466c36418882c374dba3a516b252aa966
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-09-19 22:06:46 -0300

    update selector style

--- src/components/global/LangSelector.vue
+++ src/components/global/LangSelector.vue
@@ -3,18 +3,31 @@ import { watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { SUPPORT_LOCALES as supportLocales, setI18nLanguage } from '@/i18n';
 
+const flags = {
+  en: '🇺🇸',
+  es: '🇪🇸',
+  pt: '🇵🇹',
+  it: '🇮🇹',
+};
+
 const { locale } = useI18n({ useScope: 'global' });
-watch(locale, (val) => {
-  setI18nLanguage(val);
-});
+
+function changeLanguage(lang) {
+  locale.value = lang;
+  setI18nLanguage(lang);
+}
 </script>
 
 <template>
-  <select class="App-language" v-model="locale">
-    <option
-      v-for="optionLocale in supportLocales"
-      :key="`locale-${optionLocale}`"
-      :value="optionLocale">{{ optionLocale }}
-    </option>
-  </select>
+  <li class="nav-item">
+    <a href="javascript:void(0)">{{ flags[locale] }}</a>
+    <ul class="sub-menu">
+      <li class="nav-item"
+        v-for="optionLocale in supportLocales"
+        :key="`locale-${optionLocale}`">
+        <a href="javascript:void(0)"
+          @click="changeLanguage(optionLocale)">{{ flags[optionLocale] }}</a>
+      </li>
+    </ul>
+  </li>
 </template>
