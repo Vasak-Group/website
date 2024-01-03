f770eb7429248d1b3c426c1a01449dd8c579acd1
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-01-03 11:29:48 -0300

    Refactor HeroHome.vue component

--- src/components/home/HeroHome.vue
+++ src/components/home/HeroHome.vue
@@ -1,41 +1,92 @@
 <template>
-    <section id="hero" class="hero-section">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-xl-5 col-lg-6 text-center">
-                    <div class="hero-content-wrapper">
-                        <h2 class="mb-25">
-                            {{ $t('home.welcome') }}
-                        </h2>
-                        <h1 class="pb-35 vsk-font">
-                            Vasak Group
-                        </h1>
-                        <p class="mb-35">{{ $t('home.headerText') }}</p>
-                        <RouterLink to="/contact" class="theme-btn">{{ $t('home.knowUs') }}</RouterLink>
-                    </div>
-                </div>
-                <div class="col-xl-7 col-lg-6">
-                    <div class="hero-img">
-                        <div class="d-inline-block hero-img-right">
-                            <img class="image" src="/img/head.svg" loading="lazy">
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <section class="relative py-32 lg:py-36">
+    <div
+      class="mx-auto lg:max-w-7xl w-full px-5 sm:px-10 md:px-12 lg:px-5 flex flex-col lg:flex-row gap-10 lg:gap-12"
+    >
+      <div
+        class="absolute w-full lg:w-1/2 inset-y-0 lg:right-0 hidden lg:block"
+      >
+        <span
+          class="absolute -left-6 md:left-4 top-24 lg:top-28 w-24 h-24 rotate-90 skew-x-12 rounded-3xl bg-vsk-1 blur-xl opacity-60 lg:opacity-95 lg:block hidden"
+        ></span>
+        <span
+          class="absolute right-4 bottom-12 w-24 h-24 rounded-3xl bg-vsk-2 blur-xl opacity-80"
+        ></span>
+      </div>
+      <span
+        class="w-4/12 lg:w-2/12 aspect-square bg-gradient-to-tr from-vsk-2 to-vsk-1 absolute -top-5 lg:left-0 rounded-full skew-y-12 blur-2xl opacity-40 skew-x-12 rotate-90"
+      ></span>
+      <div
+        class="relative flex flex-col items-center text-center lg:text-left lg:py-7 xl:py-8 lg:items-start lg:max-w-none max-w-3xl mx-auto lg:mx-0 lg:flex-1 lg:w-1/2"
+      >
+        <h1
+          class="text-3xl leading-tight sm:text-4xl md:text-5xl xl:text-6xl font-bold text-gray-900 dark:text-gray-100"
+        >
+          {{ $t("home.welcome") }}
+          <span
+            class="text-transparent bg-clip-text bg-gradient-to-bl from-vsk-2 to-vsk-1"
+            >Vasak Group</span
+          >
+        </h1>
+        <p class="mt-8 text-gray-700 dark:text-gray-300">
+          {{ $t("home.headerText") }}
+        </p>
+        <div class="mt-10 w-full flex max-w-md mx-auto lg:mx-0">
+          <div class="flex sm:flex-row flex-col gap-5 w-full">
+            <RouterLink
+              to="/about"
+              class="w-full sm:w-auto flex items-center justify-center px-3 py-3 border-transparent text-base font-medium rounded-xl text-white bg-gradient-to-bl from-vsk-2 to-vsk-1 hover:to-vsk-2 md:py-4 md:text-lg md:px-10"
+            >
+              {{ $t("navbar.about") }}
+            </RouterLink>
+            <RouterLink
+              to="/contact"
+              class="w-full sm:w-auto flex items-center justify-center px-6 py-3 border-transparent text-base font-medium rounded-xl text-vsk-1 bg-white hover:bg-vsk-1 hover:text-white md:py-4 md:text-lg md:px-10"
+            >
+              {{ $t("navbar.contact") }}
+            </RouterLink>
+          </div>
         </div>
-    </section>
+      </div>
+      <div
+        class="flex flex-1 lg:w-1/2 lg:h-auto relative lg:max-w-none lg:mx-0 mx-auto max-w-3xl"
+      >
+        <img
+          src="/img/head.svg"
+          alt="Hero image"
+          width="2350"
+          height="2359"
+          class="lg:absolute lg:w-full lg:h-full rounded-3xl object-cover lg:max-h-none max-h-96"
+        />
+      </div>
+    </div>
+  </section>
+  <section id="hero" class="hero-section">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-xl-5 col-lg-6 text-center">
+          <div class="hero-content-wrapper">
+            <h2 class="mb-25"></h2>
+            <h1 class="pb-35 vsk-font"></h1>
+            <p class="mb-35"></p>
+            <RouterLink to="/contact" class="theme-btn">{{
+              $t("home.knowUs")
+            }}</RouterLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup lang="ts">
-import { defineComponent } from 'vue';
-import { RouterLink } from 'vue-router';
+<script lang="ts">
+import { defineComponent } from "vue";
+import { RouterLink } from "vue-router";
 
-defineComponent({
-    name: 'HeroHome',
-    components: {
-        RouterLink,
-    },
+export default defineComponent({
+  name: "HeroHome",
+  components: {
+    RouterLink,
+  },
 });
 </script>
-
-<style></style>
