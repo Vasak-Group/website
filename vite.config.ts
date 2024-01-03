b51a0457ecee8ccab6bad5c704e9c47db52f8d61
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-01-03 11:24:11 -0300

    Add VueI18nPlugin to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import VueI18nPlugin from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: [fileURLToPath(new URL("./src/i18n/locales/**", import.meta.url))],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
