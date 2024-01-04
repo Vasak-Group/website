3f6f3886fa66a581d7c45dfe82feb8e4c4bd0f89
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-01-04 11:01:04 -0300

    Update dependencies and add fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { createApp } from "vue";
-import App from "@/App.vue";
-import router from "@/router/index";
-import i18n from "./i18n/index";
-import "../index.css";
+import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebookF, faLinkedin, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+import App from '@/App.vue';
+import router from '@/router/index';
+import i18n from './i18n/index';
+import '../index.css';
 
 const app = createApp(App);
 
+library.add(faFacebookF, faLinkedin, faInstagram, faWhatsapp, faEnvelope, faArrowUp);
+
+// @ts-ignore
 app.use(router);
+// @ts-ignore
 app.use(i18n());
 
-app.mount("#app");
+app.mount('#app');
