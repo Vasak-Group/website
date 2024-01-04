a440fa444cdc05fb2d264521607ac5fb93187a46
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-01-04 11:14:10 -0300

    Refactor import statements in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script lang="ts">
 /* eslint-disable */
-import { RouterView } from "vue-router";
-import { defineComponent } from "vue";
+import { RouterView } from 'vue-router';
+import { defineComponent } from 'vue';
 
-import FooterComponent from "@/components/FooterComponent.vue";
-import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from '@/components/FooterComponent.vue';
+import HeaderComponent from '@/components/HeaderComponent.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
     FooterComponent,
     HeaderComponent,
-    RouterView,
+    RouterView
   },
   mounted() {
     window.scrollTo(0, 0);
-  },
+  }
 });
 </script>
 
