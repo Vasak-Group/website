20d2a82b3790dc3781d0d9760a06638c144bbb0e
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-01-04 15:38:11 -0300

    Refactor feature card component and update feature component

--- src/components/home/FeatureComponent.vue
+++ src/components/home/FeatureComponent.vue
@@ -1,38 +1,42 @@
 <template>
-    <section id="feature" class="feature-section pt-130 pb-130">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-6 col-lg-7 col-md-9 mx-auto">
-                    <div class="section-title text-center mb-55">
-                        <span>{{ $t('home.features.feature') }}</span>
-                        <h2>{{ $t('home.features.question') }}</h2>
-                        <p>{{ $t('home.features.answer') }}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <FeatureCard v-for="featured in featureds" :key="featured.id" v-bind="featured" />
-            </div>
-        </div>
-    </section>
+  <div class="bg-white dark:bg-gray-800 py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:text-center">
+        <h2 class="text-base font-semibold leading-7 text-vsk-2">{{ $t('home.features.feature') }}</h2>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-vsk-1 sm:text-4xl">
+          {{ $t('home.features.question') }}
+        </p>
+        <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">
+          {{ $t('home.features.answer') }}
+        </p>
+      </div>
+      <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+        <dl
+          class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16"
+        >
+        <FeatureCard v-for="featured in featureds" :key="featured.id" v-bind="featured" />
+        </dl>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { defineComponent } from 'vue';
-import featureds from '../../data/features.json';
-import FeatureCard from '../cards/FeatureCard.vue';
+import featureds from '@/data/features';
+import FeatureCard from '@/components/cards/FeatureCard.vue';
 
-defineComponent({
-    name: 'FeatureComponent',
-    components: {
-        FeatureCard,
-    },
-    setup() {
-        return {
-            featureds,
-        };
-    },
+export default defineComponent({
+  name: 'FeatureComponent',
+  components: {
+    FeatureCard
+  },
+  setup() {
+    return {
+      featureds
+    };
+  }
 });
 </script>
 
-<style></style>
+../../data/features
