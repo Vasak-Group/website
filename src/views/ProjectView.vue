a88b56d3457f0fa0af439aed9351b19873913382
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-01-05 14:19:51 -0300

    Refactor ProjectView.vue component layout

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,56 +1,56 @@
 <template>
-  <BreadcrumbsComponent :title="project?.name" />
-  <section class="portfolio-section pt-130">
-    <div class="container">
-      <div class="row">
-
-        <div class="col-xl-8 col-lg-7">
-          <div class="left-side-wrapper">
-            <div class="single-portfolio mb-60">
-              <div class="single-portfolio-img mb-30">
-                <img :src="project?.image" :alt="project?.name" :title="project?.name" loading="lazy">
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-xl-4 col-lg-5">
-          <InfoProject :name="project?.name" :description="project?.description" :type="project?.type"
-            :client="project?.client" :url="project?.url" />
-        </div>
+  <section class="relative">
+    <div class="absolute inset-0">
+      <img
+        :src="project?.image"
+        :alt="project?.name"
+        :title="project?.name"
+        width="100%"
+        height="100%"
+        style=""
+      />
+    </div>
+    <div class="container px-5 py-24 mx-auto flex">
+      <div
+        class="lg:w-1/3 md:w-1/2 bg-white dark:bg-gray-800 rounded-xl p-8 flex flex-col md:ml-auto w-full my-16 py-20 md:mt-0 relative z-10 shadow-lg"
+      >
+        <InfoProject
+          :name="project?.name"
+          :description="project?.description"
+          :type="project?.type"
+          :client="project?.client"
+          :url="project?.url"
+        />
       </div>
     </div>
   </section>
 </template>
 
-<script setup lang="ts">
-import projects from '@/data/projects.json';
+<script lang="ts">
+import projects from '@/data/projects';
 import InfoProject from '@/components/project/InfoProject.vue';
-import BreadcrumbsComponent from '@/components/global/BreadcrumbsComponent.vue';
-import { defineComponent, onMounted } from 'vue';
+import { defineComponent } from 'vue';
 import { useRoute } from 'vue-router';
-import Project from '@/types/Project';
 
-const route = useRoute();
-const project: Project = projects.find(project => project.id === parseInt(route.params.id.toString())) || {} as Project;
-
-onMounted(() => {
-  document.title =  project.name + ' | Vasak';
-  window.scrollTo(0, 0);
-})
-
-defineComponent({
+export default defineComponent({
   name: 'ProjectView',
+  data() {
+    return {
+      projects,
+      route: useRoute()
+    };
+  },
+  computed: {
+    project() {
+      return this.projects.find((project) => project.id === Number(this.route.params.id));
+    }
+  },
   components: {
-    BreadcrumbsComponent,
     InfoProject
-  }
-})
+  },
+  mounted() {
+    document.title = this.project.name + ' | Vasak Group';
+    window.scrollTo(0, 0);
+  },
+});
 </script>
-
-<style scoped>
-img {
-  border-radius: var(--border-radius);
-  box-shadow: 0px 0 30px rgb(34 34 34 / 8%);
-}
-</style>
