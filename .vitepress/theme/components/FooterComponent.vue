62208213d2b3fc57ee15ee2dd568ce25b2459a83
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2025-11-25 22:14:36 -0300

    feat: actualizar componentes de pie de página y barra de navegación con logo SVG

--- .vitepress/theme/components/FooterComponent.vue
+++ .vitepress/theme/components/FooterComponent.vue
@@ -6,10 +6,9 @@ const { site, theme } = useData()
 
 <template>
     <footer
-        class="my-8 text-center text-sm text-slate-500 dark:text-slate-400 mx-4 rounded-xl p-4 bg-slate-100 dark:bg-slate-900">
-        <p>
-            &copy; {{ new Date().getFullYear() }} {{ site.title }}. All rights reserved.
-        </p>
+        class="mt-8 text-center text-sm text-slate-500 dark:text-slate-400 mx-4 rounded-xl p-4 bg-slate-100 dark:bg-slate-900">
+        <img src="/img/logo.svg" :alt="site.title" class="h-14 mb-2 inline-block" />
+
         <p class="mt-2">
             <a v-for="social in theme.socialLinks" :key="social.icon" :href="social.link" target="_blank"
                 rel="noopener noreferrer" class="mx-2 text-slate-600 dark:text-slate-300 hover:text-primary text-xl">
@@ -17,4 +16,11 @@ const { site, theme } = useData()
             </a>
         </p>
     </footer>
+    <div class="text-center my-4">
+        <p>
+            &copy; {{ new Date().getFullYear() }} <span class="vsk-text-gradient vsk-font">{{ site.title }}</span>. All
+            rights reserved.
+        </p>
+    </div>
+
 </template>
